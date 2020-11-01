38aea64c1e868be0b950fa979e72d4c7f2c6a4f9
Jordan Wine <jordandwine@gmail.com>
2020-11-01 11:22:48 -0800

    from spring init

---
+++ src/main/java/com/drchunks/brokerchunksfront/BrokerchunksFrontApplication.java
@@ -0,0 +1,13 @@
+package com.drchunks.brokerchunksfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerchunksFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokerchunksFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drchunks/brokerchunksfront/BrokerchunksFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drchunks.brokerchunksfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerchunksFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
